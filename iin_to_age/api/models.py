cc33143e9b7d4fd0f32d268cc719c5f2a0fcd508
Алексей Николаев <cei4bio@gmail.com>
2022-08-31 19:51:12 +0600

    Validation

--- iin_to_age/api/models.py
+++ iin_to_age/api/models.py
@@ -2,7 +2,7 @@ from django.core.exceptions import ValidationError
 from django.db import models
 
 
-def validate_unique(obj, value=12):
+def validate_length(obj, value=12):
     if len(str(obj)) < value:
         raise ValidationError("Not enough symbols(need 12).")
 
@@ -12,5 +12,5 @@ class Person(models.Model):
         max_length=12,
         verbose_name="IIN",
         unique=True,
-        validators=[validate_unique],
+        validators=[validate_length],
     )
